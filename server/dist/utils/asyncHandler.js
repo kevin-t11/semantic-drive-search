abe976156a104f0d8d6b58060ddd86000165dab4
kevin-t11 <kevthummar278@gmail.com>
2025-03-12 23:13:19 +0530

    updated server env config

--- server/dist/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = void 0;
-// A utility that wraps asynchronous functions to handle errors and avoid repetitive try/catch blocks.
-const asyncHandler = (fn) => (req, res, next) => {
-    Promise.resolve(fn(req, res, next)).catch(next);
-};
-exports.asyncHandler = asyncHandler;
